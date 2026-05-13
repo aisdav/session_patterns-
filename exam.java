bf72b1ba9315aefe81c690b3674d7279972428da
Davletkan Aisultan <aisdavletkan@gmail.com>
2026-05-13 17:41:17 +0500

    Create exam.java

---
+++ exam.java
@@ -0,0 +1,40 @@
+// Создаем абстракный класс для всех баз данных
+abstract class DB {
+    abstract void connect();
+    abstract void disconnect();
+}
+// Теперь наш mysql зависит от абстрактного класса DB
+class MySQLDatabase extends DB {
+    public void connect() {
+        System.out.println("Подключение к MySQL");
+    }
+
+    public void disconnect() {
+        System.out.println("Отключение от MySQL");
+    }
+}
+// Добавил postgresql для вариативности
+class PostgreSQLDatabase extends DB {
+    public void connect() {
+        System.out.println("Подключение к PostgreSQL");
+    }
+
+    public void disconnect() {
+        System.out.println("Отключение от PostgreSQL...");
+    }
+}
+//Теперь наш высокоуровневый код зависит от абстрактций
+class DataProcessor {
+    // Вот здесь в типах пишем абстрактный класс DB
+    private DB database;
+
+    public DataProcessor(DB database) {
+        this.database = database;
+    }
+
+    public void process() {
+        database.connect();
+        System.out.println("Обработка данных...");
+        database.disconnect();
+    }
+}
